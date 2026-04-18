cab8b4333c55f0d7180c934f4c41bb53b278623a
krivpro <krivservice@gmail.com>
2026-04-18 20:46:24 +0300

    новая версия интерфейса

--- js/script.js
+++ js/script.js
@@ -35,6 +35,15 @@ let currentSetForAddingSong = null; // Текущий сет для добавл
 let currentPerformanceSet = null; // Текущий сет в режиме выступления
 let currentSongIndex = 0; // Индекс текущей песни в выступлении
 
+const ICONS = {
+  add: `<svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M12 17V7" stroke="currentColor" stroke-width="1.5" stroke-linecap="round"/><path d="M7 12L17 12" stroke="currentColor" stroke-width="1.5" stroke-linecap="round"/></svg>`,
+  edit: `<svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" clip-rule="evenodd" d="M15.1369 3.46967C14.9963 3.32902 14.8055 3.25 14.6066 3.25C14.4077 3.25 14.2169 3.32902 14.0763 3.46967L4.88388 12.6621C4.78965 12.7563 4.72223 12.8739 4.68856 13.0028L3.68856 16.8313C3.62127 17.0889 3.69561 17.3629 3.88388 17.5511C4.07215 17.7394 4.34614 17.8138 4.60375 17.7465L8.43218 16.7465C8.56111 16.7128 8.67874 16.6454 8.77297 16.5511L17.9654 7.35876C18.2582 7.06586 18.2582 6.59099 17.9654 6.2981L15.1369 3.46967ZM6.08843 13.5788L14.6066 5.06066L16.3744 6.82843L7.8562 15.3466L5.46344 15.9716L6.08843 13.5788Z" fill="currentColor"/><path d="M4 19.25C3.58579 19.25 3.25 19.5858 3.25 20C3.25 20.4142 3.58579 20.75 4 20.75H19C19.4142 20.75 19.75 20.4142 19.75 20C19.75 19.5858 19.4142 19.25 19 19.25H4Z" fill="currentColor"/></svg>`,
+  delete: `<svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M10 2.25C9.58579 2.25 9.25 2.58579 9.25 3V3.75H5C4.58579 3.75 4.25 4.08579 4.25 4.5C4.25 4.91421 4.58579 5.25 5 5.25H19C19.4142 5.25 19.75 4.91421 19.75 4.5C19.75 4.08579 19.4142 3.75 19 3.75H14.75V3C14.75 2.58579 14.4142 2.25 14 2.25H10Z" fill="currentColor"/><path d="M10 10.65C10.4142 10.65 10.75 10.9858 10.75 11.4L10.75 18.4C10.75 18.8142 10.4142 19.15 10 19.15C9.58579 19.15 9.25 18.8142 9.25 18.4L9.25 11.4C9.25 10.9858 9.58579 10.65 10 10.65Z" fill="currentColor"/><path d="M14.75 11.4C14.75 10.9858 14.4142 10.65 14 10.65C13.5858 10.65 13.25 10.9858 13.25 11.4V18.4C13.25 18.8142 13.5858 19.15 14 19.15C14.4142 19.15 14.75 18.8142 14.75 18.4V11.4Z" fill="currentColor"/><path fill-rule="evenodd" clip-rule="evenodd" d="M5.99142 7.91718C6.03363 7.53735 6.35468 7.25 6.73684 7.25H17.2632C17.6453 7.25 17.9664 7.53735 18.0086 7.91718L18.2087 9.71852C18.5715 12.9838 18.5715 16.2793 18.2087 19.5446L18.189 19.722C18.045 21.0181 17.0404 22.0517 15.7489 22.2325C13.2618 22.5807 10.7382 22.5807 8.25108 22.2325C6.95954 22.0517 5.955 21.0181 5.81098 19.722L5.79128 19.5446C5.42846 16.2793 5.42846 12.9838 5.79128 9.71852L5.99142 7.91718ZM7.40812 8.75L7.2821 9.88417C6.93152 13.0394 6.93152 16.2238 7.2821 19.379L7.3018 19.5563C7.37011 20.171 7.84652 20.6612 8.45905 20.747C10.8082 21.0758 13.1918 21.0758 15.5409 20.747C16.1535 20.6612 16.6299 20.171 16.6982 19.5563L16.7179 19.379C17.0685 16.2238 17.0685 13.0394 16.7179 9.88417L16.5919 8.75H7.40812Z" fill="currentColor"/></svg>`,
+  play: `<svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" clip-rule="evenodd" d="M19.2661 10.4837C20.258 11.2512 20.258 12.7487 19.2661 13.5162C16.2685 15.8356 12.9213 17.6637 9.34979 18.9322L8.69731 19.1639C7.44904 19.6073 6.13053 18.7627 5.96154 17.4742C5.48938 13.8739 5.48938 10.126 5.96154 6.52574C6.13053 5.23719 7.44904 4.39263 8.69732 4.83597L9.34979 5.06771C12.9213 6.33619 16.2685 8.16434 19.2661 10.4837ZM18.3481 12.3298C18.5639 12.1628 18.5639 11.837 18.3481 11.6701C15.4763 9.44796 12.2695 7.69648 8.84777 6.4812L8.19529 6.24947C7.87034 6.13406 7.49691 6.35401 7.44881 6.72079C6.99363 10.1915 6.99363 13.8084 7.4488 17.2791C7.49691 17.6459 7.87034 17.8658 8.19529 17.7504L8.84777 17.5187C12.2695 16.3034 15.4763 14.5519 18.3481 12.3298Z" fill="currentColor"/></svg>`,
+  up: `<svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M8.53033 10.5303C8.23744 10.8232 7.76256 10.8232 7.46967 10.5303C7.17678 10.2374 7.17678 9.76256 7.46967 9.46967L11.4697 5.46967C11.7626 5.17678 12.2374 5.17678 12.5303 5.46967L16.5303 9.46967C16.8232 9.76256 16.8232 10.2374 16.5303 10.5303C16.2374 10.8232 15.7626 10.8232 15.4697 10.5303L12.75 7.81066L12.75 17.5C12.75 17.9142 12.4142 18.25 12 18.25C11.5858 18.25 11.25 17.9142 11.25 17.5L11.25 7.81066L8.53033 10.5303Z" fill="currentColor"/></svg>`,
+  down: `<svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M12.75 16.1893L15.4697 13.4697C15.7626 13.1768 16.2374 13.1768 16.5303 13.4697C16.8232 13.7626 16.8232 14.2374 16.5303 14.5303L12.5303 18.5303C12.2374 18.8232 11.7626 18.8232 11.4697 18.5303L7.46967 14.5303C7.17678 14.2374 7.17678 13.7626 7.46967 13.4697C7.76256 13.1768 8.23744 13.1768 8.53033 13.4697L11.25 16.1893L11.25 6.5C11.25 6.08579 11.5858 5.75 12 5.75C12.4142 5.75 12.75 6.08579 12.75 6.5L12.75 16.1893Z" fill="currentColor"/></svg>`
+};
+
 function getPreparedPresetSongs() {
   const presetSongs = Array.isArray(window.PRESET_SONGS) ? window.PRESET_SONGS : [];
 
@@ -280,7 +289,8 @@ function renderSpeeches() {
 
     // Кнопка добавления сета
     const addSetBtn = document.createElement('button');
-    addSetBtn.innerHTML = '➕';
+    addSetBtn.className = 'menu-icon-btn';
+    addSetBtn.innerHTML = ICONS.add;
     addSetBtn.title = 'Добавить сет';
     addSetBtn.addEventListener('click', (e) => {
       e.stopPropagation();
@@ -289,7 +299,8 @@ function renderSpeeches() {
 
     // Кнопка редактирования
     const editBtn = document.createElement('button');
-    editBtn.innerHTML = '✏️';
+    editBtn.className = 'menu-icon-btn';
+    editBtn.innerHTML = ICONS.edit;
     editBtn.title = 'Редактировать';
     editBtn.addEventListener('click', (e) => {
       e.stopPropagation();
@@ -298,7 +309,8 @@ function renderSpeeches() {
 
     // Кнопка удаления
     const deleteBtn = document.createElement('button');
-    deleteBtn.innerHTML = '🗑️';
+    deleteBtn.className = 'menu-icon-btn';
+    deleteBtn.innerHTML = ICONS.delete;
     deleteBtn.title = 'Удалить';
     deleteBtn.addEventListener('click', (e) => {
       e.stopPropagation();
@@ -311,7 +323,7 @@ function renderSpeeches() {
     // Счетчик сетов
     const counter = document.createElement('div');
     counter.className = 'menu__item-counter';
-    counter.textContent = `🗂️ ${speech.sets.length} сет${speech.sets.length !== 1 ? 'а' : ''}`;
+    counter.textContent = `Сетов: ${speech.sets.length}`;
 
     item.append(container, counter);
     elements.menuList.appendChild(item);
@@ -368,20 +380,20 @@ function renderSets(sets) {
     // Кнопка выступления
     const performBtn = document.createElement('button');
     performBtn.className = 'set-item__btn set-item__btn--perform';
-    performBtn.innerHTML = '🎤';
+    performBtn.innerHTML = ICONS.play;
     performBtn.title = 'Выступить';
     performBtn.addEventListener('click', () => startPerformance(set.id));
 
     // Кнопка редактирования
     const editBtn = document.createElement('button');
     editBtn.className = 'set-item__btn';
-    editBtn.innerHTML = '✏️';
+    editBtn.innerHTML = ICONS.edit;
     editBtn.addEventListener('click', () => openSetModal(set.id));
 
     // Кнопка удаления
     const deleteBtn = document.createElement('button');
     deleteBtn.className = 'set-item__btn';
-    deleteBtn.innerHTML = '🗑️';
+    deleteBtn.innerHTML = ICONS.delete;
     deleteBtn.addEventListener('click', () => deleteSet(set.id));
 
     actions.append(performBtn, editBtn, deleteBtn);
@@ -423,7 +435,7 @@ function renderSets(sets) {
           if (index > 0) {
             const moveUpBtn = document.createElement('button');
             moveUpBtn.className = 'set-song-item__btn';
-            moveUpBtn.innerHTML = '⬆️';
+            moveUpBtn.innerHTML = ICONS.up;
             moveUpBtn.title = 'Поднять выше';
             moveUpBtn.addEventListener('click', () => moveSongInSet(set.id, songInSet.songId, 'up'));
             songActions.appendChild(moveUpBtn);
@@ -432,7 +444,7 @@ function renderSets(sets) {
           if (index < sortedSongs.length - 1) {
             const moveDownBtn = document.createElement('button');
             moveDownBtn.className = 'set-song-item__btn';
-            moveDownBtn.innerHTML = '⬇️';
+            moveDownBtn.innerHTML = ICONS.down;
             moveDownBtn.title = 'Опустить ниже';
             moveDownBtn.addEventListener('click', () => moveSongInSet(set.id, songInSet.songId, 'down'));
             songActions.appendChild(moveDownBtn);
@@ -441,7 +453,7 @@ function renderSets(sets) {
           // Кнопка удаления из сета
           const removeBtn = document.createElement('button');
           removeBtn.className = 'set-song-item__btn';
-          removeBtn.innerHTML = '❌';
+          removeBtn.innerHTML = ICONS.delete;
           removeBtn.title = 'Удалить из сета';
           removeBtn.addEventListener('click', () => removeSongFromSet(set.id, songInSet.songId));
           songActions.appendChild(removeBtn);
@@ -654,14 +666,14 @@ function renderSongs() {
     // Кнопка редактирования
     const editBtn = document.createElement('button');
     editBtn.className = 'song-item__btn';
-    editBtn.innerHTML = '✏️';
+    editBtn.innerHTML = ICONS.edit;
     editBtn.title = 'Редактировать';
     editBtn.addEventListener('click', () => openSongEditModal(song.id));
 
     // Кнопка удаления
     const deleteBtn = document.createElement('button');
     deleteBtn.className = 'song-item__btn';
-    deleteBtn.innerHTML = '🗑️';
+    deleteBtn.innerHTML = ICONS.delete;
     deleteBtn.title = 'Удалить';
     deleteBtn.addEventListener('click', () => deleteSong(song.id));
 
@@ -806,7 +818,7 @@ function renderSetSongs(set) {
       if (index > 0) {
         const moveUpBtn = document.createElement('button');
         moveUpBtn.className = 'set-song-modal-item__btn';
-        moveUpBtn.innerHTML = '⬆️';
+        moveUpBtn.innerHTML = ICONS.up;
         moveUpBtn.title = 'Поднять выше';
         moveUpBtn.addEventListener('click', () => moveSongInSetModal(set.id, songInSet.songId, 'up'));
         songActions.appendChild(moveUpBtn);
@@ -815,7 +827,7 @@ function renderSetSongs(set) {
       if (index < sortedSongs.length - 1) {
         const moveDownBtn = document.createElement('button');
         moveDownBtn.className = 'set-song-modal-item__btn';
-        moveDownBtn.innerHTML = '⬇️';
+        moveDownBtn.innerHTML = ICONS.down;
         moveDownBtn.title = 'Опустить ниже';
         moveDownBtn.addEventListener('click', () => moveSongInSetModal(set.id, songInSet.songId, 'down'));
         songActions.appendChild(moveDownBtn);
@@ -824,7 +836,7 @@ function renderSetSongs(set) {
       // Кнопка удаления из сета
       const removeBtn = document.createElement('button');
       removeBtn.className = 'set-song-modal-item__btn set-song-modal-item__btn--remove';
-      removeBtn.innerHTML = '❌';
+      removeBtn.innerHTML = ICONS.delete;
       removeBtn.title = 'Удалить из сета';
       removeBtn.addEventListener('click', () => removeSongFromSetModal(set.id, songInSet.songId));
       songActions.appendChild(removeBtn);
